bbddab90f56aa4e21c3ee52d5b7d3abb7d115e2b
Fabian Stevens Varon <fabianchoxd@gmail.com>
2015-01-28 01:29:11 -0500

    Fabian - Add About RNR

---
+++ web/js/main.js
@@ -0,0 +1,62 @@
+
+
+// Document ready
+$(function() {
+
+/*-----------------------------------------------------------------------------------*/
+/*	02. NAVBAR STICKY + SELECTED
+/*-----------------------------------------------------------------------------------*/
+	
+
+var cbpAnimatedHeader = (function() {
+
+	var docElem = document.documentElement,
+		header = document.querySelector( '.cbp-af-header' ),
+		didScroll = false,
+		changeHeaderOn = 10;
+
+	function init() {
+		window.addEventListener( 'scroll', function( event ) {
+			if( !didScroll ) {
+				didScroll = true;
+				setTimeout( scrollPage, 100 );
+			}
+		}, false );
+	}
+
+	function scrollPage() {
+		var sy = scrollY();
+		if ( sy >= changeHeaderOn ) {
+			classie.add( header, 'cbp-af-header-shrink' );
+		}
+		else {
+			classie.remove( header, 'cbp-af-header-shrink' );
+		}
+		didScroll = false;
+	}
+
+	function scrollY() {
+		return window.pageYOffset || docElem.scrollTop;
+	}
+
+	init();
+
+})();
+
+/*-----------------------------------------------------------------------------------*/
+/*	06. RESPONSIVE MENU
+/*-----------------------------------------------------------------------------------*/
+
+		jQuery("#collapse").hide();
+		jQuery("#collapse-menu").on("click", function () {
+		
+		    jQuery("#collapse").slideToggle(300);
+		    return false;
+		    
+		}, function () {
+		    
+		    jQuery("#collapse").slideToggle(300);
+		    return false;
+		});
+
+});
