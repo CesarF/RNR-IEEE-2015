a329843ba66c504b3f72e2d94c48dae8b66d7c38
Fabian Stevens Varon <fabianchoxd@gmail.com>
2015-01-31 02:24:12 -0500

    Fabian - Still Working Schedule's *Show Only 1*
    Hey i need put only 1 Schedule... and hide the others...

--- web/js/main.js
+++ web/js/main.js
@@ -61,22 +61,42 @@ var cbpAnimatedHeader = (function() {
 
 });
 // Oculta y muestra Div agenda
-function mostrar() {
+function mostrar1() {
           div1 = document.getElementById('Schedule1');
-          div2 = document.getElementById('Schedule2');
-          div3 = document.getElementById('Schedule3');
-
-          if ( div1.style.display == 'none')     {        
-            
+          if ( div1.style.display == 'none')
+               {                    
             	div1.style.display = 'block';
-            	div2.style.display = 'none';
-            	div3.style.display = 'none';
+          	   }
 
-           }
           else {             
             div1.style.display = 'none';
           }
         }
+
+function mostrar2() {
+          div2 = document.getElementById('Schedule2');
+          if ( div2.style.display == 'none')
+               {                    
+            	div2.style.display = 'block';
+          	   }
+          	   
+          else {             
+            div2.style.display = 'none';
+          }
+        }
+
+function mostrar3() {
+          div3 = document.getElementById('Schedule3');
+          if ( div3.style.display == 'none')
+               {                    
+            	div3.style.display = 'block';
+          	   }
+          	   
+          else {             
+            div3.style.display = 'none';
+          }
+        }
+
 
  // Slider script 
  $(function () {
