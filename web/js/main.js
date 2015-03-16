939360b8253778cb1effaabbba451d8c9c8076fe
Fabian Stevens Varon <fabianchoxd@gmail.com>
2015-03-16 01:50:55 -0500

    Fabian - Add img Conferencistas
    Add so many Styles
    Add Schedule Animation
    Add Button Animation
    Others Animations and Stlyes.

--- web/js/main.js
+++ web/js/main.js
@@ -10,6 +10,9 @@ $(document).ready(function() {
         $("#contenedor_pago").load('head.html #btn_pago');
       });
 $(document).ready(function() {        
+        $("#contenedor_pago_lugar").load('head.html #btn_pago');
+      });
+$(document).ready(function() {        
         $("#interblock_footer").load('head.html #footer_container');
       });
 // Document ready
@@ -73,34 +76,88 @@ var cbpAnimatedHeader = (function() {
 
 });
 // Oculta y muestra Div agenda
+
+// Color Gris #6E6B6B
+
+// $(document).ready(function(){ 
+//    $("mostrar1").click(function(){
+//       $(".AgendaArt2").css("background", "");
+//    });
+// });
+
+
+$(document).ready(function() {        
+
+		$(".AgendaArt2").addClass("AgendaArt2_Gris");
+        $(".AgendaArt2").removeClass("AgendaArt2");
+        $(".AgendaArt3").addClass("AgendaArt3_Gris");
+        $(".AgendaArt3").removeClass("AgendaArt3");
+
+      });
+
 function mostrar1() {
-          div1 = document.getElementById('Schedule1');
-          div2 = document.getElementById('Schedule2');          
-          div3 = document.getElementById('Schedule3');
 
-          if ( div1.style.display == 'none')
-               {                    
-            	div1.style.display = 'block';
-            	div2.style.display = 'none';
-            	div3.style.display = 'none';          	   }
+	        div1 = document.getElementById('Schedule1');
+	        div2 = document.getElementById('Schedule2');          
+	        div3 = document.getElementById('Schedule3');
 
-          else {             
+	        $(".AgendaArt1_Gris").addClass("AgendaArt1");            
+	      	$(".AgendaArt1_Gris").removeClass("AgendaArt1_Gris");
+	        $(".AgendaArt2_Gris").addClass("AgendaArt2");            
+	      	$(".AgendaArt2_Gris").removeClass("AgendaArt2_Gris");
+	        $(".AgendaArt3_Gris").addClass("AgendaArt3");            
+	        $(".AgendaArt3_Gris").removeClass("AgendaArt3_Gris");
+		
+		  if ( div1.style.display == 'none')
+
+            {                              
+
+	        $(".AgendaArt2").addClass("AgendaArt2_Gris");
+	        $(".AgendaArt2").removeClass("AgendaArt2");
+	        $(".AgendaArt3").addClass("AgendaArt3_Gris");
+	        $(".AgendaArt3").removeClass("AgendaArt3");
+
+        	div1.style.display = 'block';
+        	div2.style.display = 'none';
+        	div3.style.display = 'none';          	   
+           	}
+
+          else {  
             div1.style.display = 'none';
             div2.style.display = 'none';
             div3.style.display = 'none';  
-          }
+          		}
         }
 
 function mostrar2() {
+
 		div1 = document.getElementById('Schedule1');
         div2 = document.getElementById('Schedule2');          
         div3 = document.getElementById('Schedule3');
 
+
+	        $(".AgendaArt1_Gris").addClass("AgendaArt1");
+	        $(".AgendaArt1_Gris").removeClass("AgendaArt1_Gris");
+	        $(".AgendaArt2_Gris").addClass("AgendaArt2");
+	        $(".AgendaArt2_Gris").removeClass("AgendaArt2_Gris");
+	        $(".AgendaArt3_Gris").addClass("AgendaArt3");
+	        $(".AgendaArt3_Gris").removeClass("AgendaArt3_Gris");
+            
+
           if ( div2.style.display == 'none')
-               {                    
-            	div1.style.display = 'none';
-            	div2.style.display = 'block';
-            	div3.style.display = 'none';  
+
+               {
+
+            $(".AgendaArt1").addClass("AgendaArt1_Gris");
+	        $(".AgendaArt1").removeClass("AgendaArt1");
+	        $(".AgendaArt2").addClass("AgendaArt2");
+	        $(".AgendaArt2").removeClass("AgendaArt2_Gris");
+	        $(".AgendaArt3").addClass("AgendaArt3_Gris");
+	        $(".AgendaArt3").removeClass("AgendaArt3");
+        	
+        	div1.style.display = 'none';
+        	div2.style.display = 'block';
+            div3.style.display = 'none';  
           	   }
           	   
           else {             
@@ -111,11 +168,32 @@ function mostrar2() {
         }
 
 function mostrar3() {
+
           div1 = document.getElementById('Schedule1');
           div2 = document.getElementById('Schedule2'); 
           div3 = document.getElementById('Schedule3');
+
+
+	        $(".AgendaArt1_Gris").addClass("AgendaArt1");
+	        $(".AgendaArt1_Gris").removeClass("AgendaArt1_Gris");
+	        $(".AgendaArt2_Gris").addClass("AgendaArt2");
+	        $(".AgendaArt2_Gris").removeClass("AgendaArt2_Gris");
+	        $(".AgendaArt3_Gris").addClass("AgendaArt3");
+	        $(".AgendaArt3_Gris").removeClass("AgendaArt3_Gris");
+
+
           if ( div3.style.display == 'none')
-               {                    
+
+               {         
+
+            $(".AgendaArt1").addClass("AgendaArt1_Gris");
+	        $(".AgendaArt1").removeClass("AgendaArt1");
+	        $(".AgendaArt2").addClass("AgendaArt2_Gris");
+	        $(".AgendaArt2").removeClass("AgendaArt2");
+	        $(".AgendaArt3").addClass("AgendaArt3");
+	        $(".AgendaArt3").removeClass("AgendaArt3_Gris");           
+
+
             	div1.style.display = 'none';
             	div2.style.display = 'none';
             	div3.style.display = 'block'; 
@@ -127,6 +205,35 @@ function mostrar3() {
             div3.style.display = 'none'; 
           }
         }
+
+//Sweet Scroll Efect.
+
+ $(function(){
+
+     $('a[href*=#]').click(function() {
+
+     if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'')
+         && location.hostname == this.hostname) {
+
+             var $target = $(this.hash);
+
+             $target = $target.length && $target || $('[name=' + this.hash.slice(1) +']');
+
+             if ($target.length) {
+
+                 var targetOffset = $target.offset().top;
+
+                 $('html,body').animate({scrollTop: targetOffset}, 1000);
+
+                 return false;
+
+            }
+
+       }
+
+   });
+
+});
 
 
  // Slider script 
@@ -164,3 +271,12 @@ function mostrar3() {
 				});
 
 			});
+
+ 		$(document).ready(function(){ 
+   $('#biografia_1_btn').on('click',function(){
+      $('#bigrafia_1_div').toggle('slow');
+   });
+   $('#biografia_2_btn').on('click',function(){
+      $('#bigrafia_2_div').toggle('slow');
+   });
+});
